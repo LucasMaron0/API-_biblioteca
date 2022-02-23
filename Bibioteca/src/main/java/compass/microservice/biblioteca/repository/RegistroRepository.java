9d3bf56fe23450755ba28607fe5b796fdefaf92f
ChristianMartins30819 <97145298+ChristianMartins30819@users.noreply.github.com>
2022-02-23 09:34:38 -0300

    Implementação da busca de registros por status
    Implementação da busca de registros por status

--- Bibioteca/src/main/java/compass/microservice/biblioteca/repository/RegistroRepository.java
+++ Bibioteca/src/main/java/compass/microservice/biblioteca/repository/RegistroRepository.java
@@ -2,13 +2,18 @@ package compass.microservice.biblioteca.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import compass.microservice.biblioteca.modelos.Livro;
 import compass.microservice.biblioteca.modelos.Registro;
+import compass.microservice.biblioteca.modelos.StatusRegistro;
 
 public interface RegistroRepository extends JpaRepository<Registro, Long> {
 
 	Optional<Registro> findByIdUsuario(Long idUser);
+
+	Page<Registro> findByStatusRegistro(StatusRegistro statusRegistro, Pageable paginacao);
 
 }
