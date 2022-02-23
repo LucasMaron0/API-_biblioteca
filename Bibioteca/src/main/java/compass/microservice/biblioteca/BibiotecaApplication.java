4da6c91a40b70298d01b1db7cadd819b67ff0353
LucasMaron0 <lucas.maron.pb@compasso.com.br>
2022-02-23 10:22:19 -0300

    Testando github

--- Bibioteca/src/main/java/compass/microservice/biblioteca/BibiotecaApplication.java
+++ Bibioteca/src/main/java/compass/microservice/biblioteca/BibiotecaApplication.java
@@ -15,6 +15,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class BibiotecaApplication {
 	
 	
+	//teste github 
+	
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
